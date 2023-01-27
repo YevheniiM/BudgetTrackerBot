a7966b23d57cf84032f149331f813719a2d2ce62
YevheniiM <xmolodtsov@gmail.com>
2023-01-27 10:14:28 +0100

    Temp print the key

--- excel/core/sheet_manager.py
+++ excel/core/sheet_manager.py
@@ -6,6 +6,8 @@ from gspread import WorksheetNotFound
 from django.conf  import settings
 
 
+from pprint import pprint
+pprint(settings.GOOGLE_AUTH_DICT)
 gc = gspread.service_account_from_dict(settings.GOOGLE_AUTH_DICT)
 
 
