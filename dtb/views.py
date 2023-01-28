58bf0fa594901bffae77f2bfdeb9840679620429
YevheniiM <xmolodtsov@gmail.com>
2023-01-28 16:58:56 +0100

    Scale worker

--- dtb/views.py
+++ dtb/views.py
@@ -23,19 +23,8 @@ def index(request):
 
 
 class TelegramBotWebhookView(View):
-    # WARNING: if fail - Telegram webhook will be delivered again.
-    # Can be fixed with async celery task execution
     def post(self, request, *args, **kwargs):
         process_telegram_event.delay(json.loads(request.body))
-        # if DEBUG:
-        #     process_telegram_event(json.loads(request.body))
-        # else:
-        #     # Process Telegram event in Celery worker (async)
-        #     # Don't forget to run it and & Redis (message broker for Celery)!
-        #     # Locally, You can run all of these services via docker-compose.yml
-        #     process_telegram_event.delay(json.loads(request.body))
-
-        # e.g. remove buttons, typing event
         return JsonResponse({"ok": "POST request processed"})
 
     def get(self, request, *args, **kwargs):  # for debug
--- tgbot/handlers/onboarding/keyboards.py
+++ tgbot/handlers/onboarding/keyboards.py
@@ -5,7 +5,6 @@ from tgbot.handlers.onboarding.manage_data import CHOOSE_CATEGORY_BUTTON
 
 def make_keyboard_for_start_command() -> InlineKeyboardMarkup:
     buttons = [[
-        InlineKeyboardButton('Excel Document', url="https://github.com/ohld/django-telegram-bot"),
         InlineKeyboardButton('Enter Expense', callback_data=f'{CHOOSE_CATEGORY_BUTTON}')
     ]]
 
